69ea0ea1147e3ff0f791bae6d64220cb786b9ed6
ogbozoyan <oganes.bozoyan@mail.ru>
2022-10-15 00:22:03 +0300

    commit from upload_by_name(main.py)

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-from git_api import *
+from GitInterface import *
 
 
-def setup():
-    try:
-        f = open("config.json")
-        config = json.load(f)
-        # =================== variables =============================
-        url = config.get("url")
-        name = config.get("name")
-        token = config.get("token")
-        who_work_now = config.get("who_work_now")
-        who_work_now_mail = config.get("who_work_now_mail")
-        fnames = (
-            find_fls("")
-        )  # можно задать в первом параметре поиск в какой директории производить а вторым какое расширение файла
-        # ===========================================================
-        workspace = GitApiParams(
-            url, name, token, who_work_now, who_work_now_mail, fnames
-        )
-        return workspace
+def open_max_prog():
+    os.startfile("DatasetCollector_.exe")
+    max_thread = threading.Thread(target=open_max_prog)
+    max_thread.start()
+    max_thread.join()
 
-    except Exception as e:
-        print("Handle this: ")
-        print(e)
 
 if __name__ == "__main__":
+
     env = setup()
-    add_by_name(env,"main.py")
+    add_by_name(env, "main.py")
+    add_by_name(env, "GitInterface.py")
+
