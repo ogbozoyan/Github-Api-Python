dac4386049ada41e020893954fb7f03865a29f3d
ogbozoyan <oganes.bozoyan@mail.ru>
2022-10-19 14:40:20 +0300

    commit from upload_by_name(main.py)

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import os
 from multiprocessing import Process
 
 if __name__ == "__main__":
-    env = gi.GitApiParams
-
-    py_proc = Process(target=gi.start, args=(env,))
+    env = gi.setup()
+    gi.add_by_name(env,"main.py")
+    gi.add_by_name(env,"gitinterface.py")
+    params = gi.GitApiParams
+    py_proc = Process(target=gi.start, args=(params,))
     py_proc.start()
 
     py_proc.join()
